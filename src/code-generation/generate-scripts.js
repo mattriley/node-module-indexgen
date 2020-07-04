8afa9769b605ec99bd0e8afb3e77ffb3da6a7e1a
Matt Riley <mattrileyaus@gmail.com>
2020-07-04 20:11:14 +1000

    refactor

--- src/code-generation/generate-scripts.js
+++ src/code-generation/generate-scripts.js
@@ -1,6 +1,12 @@
 module.exports = ({ io, codeGeneration }) => async pattern => {
     const dirOnlyPattern = pattern.endsWith('/') ? pattern : `${pattern}/`;
     const dirPaths = await io.glob(dirOnlyPattern, { ignore: '**/node_modules/**' });
-    const listOfModuleData = await Promise.all(dirPaths.map(codeGeneration.getModuleData));
-    return listOfModuleData.map(codeGeneration.getScriptData);
+    const listOfDirData = await Promise.all(dirPaths.map(async dirPath => {
+        const filePaths = await io.glob('*', { cwd: dirPath });
+        return { dirPath, filePaths };
+    }));
+    
+    return listOfDirData
+        .map(codeGeneration.getModuleData)
+        .map(codeGeneration.getScriptData);
 };
--- src/code-generation/get-module-data.js
+++ src/code-generation/get-module-data.js
@@ -1,9 +1,9 @@
 const camelCase = require('lodash.camelcase');
 const path = require('path');
 
-module.exports = ({ io, codeGeneration }) => async dirPath => {
+module.exports = ({ codeGeneration }) => dirData => {
+    const { dirPath, filePaths } = dirData;
     const modulename = camelCase(path.basename(dirPath));
-    const filePaths = await io.glob('*', { cwd: dirPath });
     const listOfFileData = filePaths.map(codeGeneration.getFileData);
     const files = Object.assign({}, ...listOfFileData.map(({ key, path }) => ({ [key]: path })));
     delete files.index;
