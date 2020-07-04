359e2c35e8ad068f44ec4517bb9717d6b311c178
Matt Riley <mattrileyaus@gmail.com>
2020-07-04 19:53:36 +1000

    refactor

--- src/code-generation/get-file-data.js
+++ src/code-generation/get-file-data.js
@@ -1,6 +1,6 @@
 const camelCase = require('lodash.camelcase');
 
-const dropLeadingUnderscoresAndDigits = str => str.match(/^_?(\d+)?(.+)/)[2];
+const dropLeadingUnderscoresAndDigits = str => str.match(/^(_+)?(\d+)?(.+)/)[3];
 
 module.exports = ({ util }) => filePath => {
     const fileNameWithoutExt = util.getFileNameWithoutExt(filePath);
