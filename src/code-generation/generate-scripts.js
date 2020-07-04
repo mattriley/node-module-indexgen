ec400a7dcee8266f0426e7a9ee03ca83929b2fa0
Matt Riley <mattrileyaus@gmail.com>
2020-07-04 18:05:57 +1000

    refactor

--- src/code-generation/generate-scripts.js
+++ src/code-generation/generate-scripts.js
@@ -1,13 +1,6 @@
 module.exports = ({ io, codeGeneration, provider }) => async pattern => {
     const dirOnlyPattern = pattern.endsWith('/') ? pattern : `${pattern}/`;
-    const unfilteredDirPaths = await io.glob(dirOnlyPattern, { ignore: '**/node_modules/**' });
-
-    // remove paths that don't contain .js files.
-    const dirPaths = await Promise.all(unfilteredDirPaths.filter(async dirPath => {
-        const filePaths = await io.glob('**/*.js', { cwd: dirPath, ignore: '**/index.js' });
-        return filePaths.length > 0;
-    }));
-
+    const dirPaths = await io.glob(dirOnlyPattern, { ignore: '**/node_modules/**' });
     const moduleInfos = await Promise.all(dirPaths.map(codeGeneration.getModuleInfo));
     return moduleInfos.map(moduleInfo => ({ ...moduleInfo, ...provider.generateScript(moduleInfo) }));
 };
