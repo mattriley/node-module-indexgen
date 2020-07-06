a076ec7467dd6eb8d54fe5a32b88f898c6cd2c90
Matt Riley <mattrileyaus@gmail.com>
2020-07-06 15:25:37 +1000

    bug fix

--- initialise.js
+++ initialise.js
@@ -1,10 +1,10 @@
-const { init } = require('@mattriley/module-initialiser');
+const compose = require('module-composer');
 const config = require('./config');
 const src = require('./src');
 
 module.exports = ({ io }) => {
     const provider = src.providers.cjs;
-    const util = init(src.util, {});
-    const codeGeneration = init(src.codeGeneration, { config, io, util, provider });
+    const util = compose(src.util, {});
+    const codeGeneration = compose(src.codeGeneration, { config, io, util, provider });
     return { codeGeneration };
 };
