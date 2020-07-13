3dd6c693e8cf7916a0bd492e8831018798b215b8
Matt Riley <mattrileyaus@gmail.com>
2020-07-13 16:48:14 +1000

    only write file if changed, output changed filed, debounce watcher

--- src/code-generation/watch.js
+++ src/code-generation/watch.js
@@ -1,8 +1,23 @@
 const path = require('path');
 
+const IN_A_SEC = 1000;
+
 module.exports = ({ codeGeneration, io }) => (targetDir, ext) => {
-    io.fs.watch(targetDir, { recursive: true }, (eventType, filename) => {
-        if (path.basename(filename) === `index.${ext}`) return;
-        codeGeneration.generateFiles(targetDir, ext);
+    
+    let timeoutId = null;
+
+    const generateFiles = delay => {
+        if (timeoutId) return;
+        timeoutId = setTimeout(() => {
+            clearTimeout(timeoutId);
+            timeoutId = null;
+            codeGeneration.generateFiles(targetDir, ext);
+        }, delay);
+    };
+
+    io.fs.watch(targetDir, { recursive: true }, (eventType, filename) => { 
+        if (path.basename(filename) === `index.${ext}`) return;        
+        generateFiles(IN_A_SEC);        
     });    
+    
 };
--- src/code-generation/write-scripts.js
+++ src/code-generation/write-scripts.js
@@ -1,8 +1,23 @@
 const path = require('path');
 
 module.exports = ({ io }) => (scriptDataList, ext) => {
-    return Promise.all(scriptDataList.map(scriptData => {
+    return Promise.all(scriptDataList.map(async scriptData => {
         const filePath = path.join(scriptData.dirPath, `index.${ext}`);
-        return io.fs.promises.writeFile(filePath, scriptData.script);
+
+        try {            
+            const exists = await io.fs.promises.access(filePath, io.fs.F_OK).then(() => true).catch(() => false);
+    
+            if (exists) {
+                const currentScript = await io.fs.promises.readFile(filePath, 'utf-8');
+                if (currentScript === scriptData.script) return;
+            }
+            
+            await io.fs.promises.writeFile(filePath, scriptData.script);
+            console.log(`${exists ? 'Regenerated' : '  Generated'} ${filePath}`);
+        } catch (err) {
+            console.log(`      Error ${filePath}`);
+            console.error(err);
+        }
+        
     }));
 };
