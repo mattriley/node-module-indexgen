ec9c1cf36d126c593f7b7351d75d9bb07510d80d
Matt Riley <mattrileyaus@gmail.com>
2020-07-24 10:35:10 +1000

    bug fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const initialise = require('./initialise');
+const initialise = require('./src/initialise');
 const { codeGeneration } = initialise();
 const { indexgen, watch } = codeGeneration.getCommands();
 module.exports = { indexgen, watch };
