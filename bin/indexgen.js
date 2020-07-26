169bf9aba234277f444875a2526bd041a0daedf3
Matt Riley <mattrileyaus@gmail.com>
2020-07-26 22:01:33 +1000

    output paths

--- bin/indexgen.js
+++ bin/indexgen.js
@@ -3,7 +3,14 @@
 const minimist = require('minimist');
 const { indexgen, watch } = require('..');
 const args = minimist(process.argv.slice(2));
-const targetDirs = args._.length ? args._.filter(dir => Boolean(dir)) : ['src'];
+const paths = args._.length ? args._.filter(dir => Boolean(dir)) : ['./src'];
+
+const lines = [
+    args.watch ? 'Watching:' : 'Paths:',
+    ...paths.map(p => `  ${p}`)
+];
+
+console.log(lines.join('\n'));
 const { ext } = args;
-targetDirs.forEach(dir => indexgen(dir, ext));
-if (args.watch) targetDirs.forEach(dir => watch(dir, ext));
+paths.forEach(dir => indexgen(dir, ext));
+if (args.watch) paths.forEach(dir => watch(dir, ext));
