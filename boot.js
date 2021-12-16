774354eb6dc1b392ebe89339cbf816026da81d21
Matt Riley <mattrileyaus@gmail.com>
2021-12-17 09:21:53 +1100

    upgrade module-composer

--- boot.js
+++ boot.js
@@ -1,9 +1,9 @@
-const merge = require('lodash.merge');
-const composeSrc = require('module-composer')(require('./src'));
+const src = require('./src');
+const composer = require('module-composer');
 
 module.exports = (overrides = {}) => {
 
-    const compose = (key, arg) => merge(composeSrc(key, arg), overrides[key]);
+    const compose = composer(src, {}, overrides);
 
     const config = compose('config');
     const io = compose('io');
@@ -11,7 +11,7 @@ module.exports = (overrides = {}) => {
     const provider = providers.cjs;
     const util = compose('util');
     const codeGeneration = compose('codeGeneration', { config, io, util, provider });
-    
+
     return { codeGeneration };
 
 };
