aa7e2bb83a3a060e30df6f097a6852a0445b7bf8
Matt Riley <mattrileyaus@gmail.com>
2022-04-16 08:32:10 +1000

    stop auto camel casing

--- src/modules/code-generation/get-file-data.js
+++ src/modules/code-generation/get-file-data.js
@@ -1,10 +1,11 @@
-const camelCase = require('lodash.camelcase');
+// const camelCase = require('lodash.camelcase');
 
 const dropLeadingUnderscoresAndDigits = str => str.match(/^(_+)?(\d+)?(.+)/)[3];
 
 module.exports = ({ util }) => filePath => {
     const basenameWithoutExt = util.getBasenameWithoutExt(filePath);
-    const key = camelCase(dropLeadingUnderscoresAndDigits(basenameWithoutExt));
+    // const key = camelCase(dropLeadingUnderscoresAndDigits(basenameWithoutExt));
+    const key = dropLeadingUnderscoresAndDigits(basenameWithoutExt);
     const path = `./${basenameWithoutExt}`;
     return { key, path };
 };
