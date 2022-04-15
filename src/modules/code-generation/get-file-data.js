b3a923acd6fceafe0e2ebfe68c93de846f54e60e
Matt Riley <mattrileyaus@gmail.com>
2022-04-16 08:42:27 +1000

    preserve starting uppercase

--- src/modules/code-generation/get-file-data.js
+++ src/modules/code-generation/get-file-data.js
@@ -1,11 +1,13 @@
-// const camelCase = require('lodash.camelcase');
+const camelCase = require('lodash.camelcase');
 
+const firstIsUpper = str => str[0] === str[0].toUpperCase();
+const upperFirst = str => str.charAt(0).toUpperCase() + str.slice(1);
 const dropLeadingUnderscoresAndDigits = str => str.match(/^(_+)?(\d+)?(.+)/)[3];
 
 module.exports = ({ util }) => filePath => {
     const basenameWithoutExt = util.getBasenameWithoutExt(filePath);
-    // const key = camelCase(dropLeadingUnderscoresAndDigits(basenameWithoutExt));
-    const key = dropLeadingUnderscoresAndDigits(basenameWithoutExt);
+    const keyCamel = camelCase(dropLeadingUnderscoresAndDigits(basenameWithoutExt));
+    const key = firstIsUpper(basenameWithoutExt) ? upperFirst(keyCamel) : keyCamel;
     const path = `./${basenameWithoutExt}`;
     return { key, path };
 };
