ce1507164be405fc3139372eaf364dd9b07768ec
Matt Riley <mattrileyaus@gmail.com>
2022-04-16 08:13:15 +1000

    Unspecified changes

--- src/modules/code-generation/get-commands.js
+++ src/modules/code-generation/get-commands.js
@@ -1,12 +1,12 @@
 module.exports = ({ config, codeGeneration }) => () => {
     const exec = (command, args) => {
-        const targetDir = args.dir || config.defaults.dir;
-        const ext = args.ext || config.defaults.ext;
+        const targetDir = args.dir || config.dir;
+        const ext = args.ext || config.ext;
         command(targetDir, ext);
     };
 
     const indexgen = (dir, ext) => exec(codeGeneration.indexgen, { dir, ext });
     const watch = (dir, ext) => exec(codeGeneration.watch, { dir, ext });
-    
+
     return { indexgen, watch };
 };
