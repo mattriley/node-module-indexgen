e7e0f73f9eef65107b7535b4d92f8156579ba148
Matt Riley <mattrileyaus@gmail.com>
2022-04-18 20:49:05 +1000

    bug fix

--- src/modules/code-generation/list-dir.js
+++ src/modules/code-generation/list-dir.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = ({ config, io }) => async targetDir => {
     const pattern = path.join(targetDir, '**/');
-    const dirPaths = await io.glob(pattern, { ignore: config.ignore });
+    const dirPaths = await io.glob(pattern, { ignore: config.ignore, onlyDirectories: true });
     return Promise.all(dirPaths.map(async dirPath => {
         const childDirPaths = await io.glob('*/', { cwd: dirPath });
         const childFilePaths = await io.glob('*.{js,cjs}', { cwd: dirPath });
