731cfdf79721073c76b6cf17141513330ca721f7
Matt Riley <mattrileyaus@gmail.com>
2022-04-24 20:48:08 +1000

    Unspecified changes

--- bin/indexgen.js
+++ bin/indexgen.js
@@ -2,8 +2,8 @@
 
 const minimist = require('minimist');
 const configure = require('../src/configure');
-const args = minimist(process.argv.slice(2));
-const paths = args._.flatMap(p => p.split(','));
+const { _: pathList, ...args } = minimist(process.argv.slice(2));
+const paths = pathList.flatMap(p => p.split(','));
 
 const parts = [
     args.watch ? 'Watching' : 'Generating',
