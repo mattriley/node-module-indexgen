6e1a7261f6287bfdc66935c6cbd528831236bef5
Matt Riley <mattrileyaus@gmail.com>
2022-04-25 18:04:16 +1000

    Unspecified changes

--- src/modules/code-generation/list-dir.js
+++ src/modules/code-generation/list-dir.js
@@ -1,10 +1,11 @@
 module.exports = ({ config, io }) => async targetDir => {
-    const pattern = `${targetDir}/**`;
-    const dirPaths = await io.glob(pattern, { ignore: config.ignore, onlyDirectories: true });
+
+    const dirPaths = await io.glob(`${targetDir}/**`, { ignore: config.ignore, onlyDirectories: true });
     return Promise.all([targetDir, ...dirPaths].map(async dirPath => {
-        const childDirPaths = await io.glob('*/', { cwd: dirPath, onlyDirectories: true });
-        const childFilePaths = await io.glob('*.{js,json}', { cwd: dirPath });
+        const childDirPaths = await io.glob('*', { cwd: dirPath, onlyDirectories: true });
+        const childFilePaths = await io.glob(config.only, { cwd: dirPath, onlyFiles: true, globstar: false });
         const filePaths = childDirPaths.concat(childFilePaths);
         return { dirPath, filePaths };
     }));
+
 };
