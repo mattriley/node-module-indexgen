25c1ac95d4d1e6c5a46af4a6c4e446e17bc055d9
Matt Riley <mattrileyaus@gmail.com>
2022-04-25 22:39:56 +1000

    refactor

--- src/modules/code-generation/get-file-data.js
+++ src/modules/code-generation/get-file-data.js
@@ -1,13 +1,13 @@
-const p = require('path');
+const path = require('path');
 const camelCase = require('lodash.camelcase');
 
 module.exports = ({ util, config }) => ({ childPath }) => {
     const isDir = childPath.endsWith('/');
-    const ext = p.extname(childPath);
+    const ext = path.extname(childPath);
     const basenameWithoutExt = util.getBasenameWithoutExt(childPath);
     const keyCamel = camelCase(util.trimLeadingUnderscoresAndDigits(basenameWithoutExt));
     const key = util.startsWithUpper(basenameWithoutExt) ? basenameWithoutExt : keyCamel;
     const pathWithoutExt = `./${ext === '.json' ? childPath : basenameWithoutExt}`;
-    const path = config.fullySpecified ? (isDir ? `./${childPath}${config.filename}` : `./${childPath}`) : pathWithoutExt;
-    return { key, path };
+    const importPath = config.fullySpecified ? (isDir ? `./${childPath}${config.filename}` : `./${childPath}`) : pathWithoutExt;
+    return { key, importPath };
 };
--- src/modules/code-generation/get-module-data.js
+++ src/modules/code-generation/get-module-data.js
@@ -2,7 +2,7 @@ module.exports = ({ codeGeneration }) => dirData => {
 
     const { dirPath, childPaths } = dirData;
     const fileDataList = childPaths.map(childPath => codeGeneration.getFileData({ childPath }));
-    const files = Object.fromEntries(fileDataList.map(({ key, path }) => [key, path]));
+    const files = Object.fromEntries(fileDataList.map(({ key, importPath }) => [key, importPath]));
     delete files.index;
     return { dirPath, files };
 
