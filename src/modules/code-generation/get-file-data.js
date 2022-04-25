9fb2cc8063959674af5abc8ed5d084ccadde554e
Matt Riley <mattrileyaus@gmail.com>
2022-04-25 20:06:08 +1000

    Unspecified changes

--- src/modules/code-generation/get-file-data.js
+++ src/modules/code-generation/get-file-data.js
@@ -6,13 +6,13 @@ const firstIsUpper = str => str[0] === str[0].toUpperCase();
 const upperFirst = str => str.charAt(0).toUpperCase() + str.slice(1);
 const dropLeadingUnderscoresAndDigits = str => str.match(/^(_+)?(\d+)?(.+)/)[3];
 
-module.exports = ({ util, config, io }) => ({ filePath, dirPath }) => {
-    const isDir = io.fs.lstatSync(p.join(dirPath, filePath)).isDirectory();
+module.exports = ({ util, config }) => ({ filePath }) => {
+    const isDir = filePath.endsWith('/');
     const ext = p.extname(filePath);
     const basenameWithoutExt = util.getBasenameWithoutExt(filePath);
     const keyCamel = camelCase(dropLeadingUnderscoresAndDigits(basenameWithoutExt));
     const key = firstIsUpper(basenameWithoutExt) ? upperFirst(keyCamel) : keyCamel;
     const pathWithoutExt = `./${ext === '.json' ? filePath : basenameWithoutExt}`;
-    const path = config.fullySpecified ? (isDir ? `./${filePath}/${config.filename}` : `./${filePath}`) : pathWithoutExt;
+    const path = config.fullySpecified ? (isDir ? `./${filePath}${config.filename}` : `./${filePath}`) : pathWithoutExt;
     return { key, path };
 };
--- src/modules/code-generation/list-dir.js
+++ src/modules/code-generation/list-dir.js
@@ -2,7 +2,7 @@ module.exports = ({ config, io }) => async targetDir => {
 
     const dirPaths = await io.glob(`${targetDir}/**`, { ignore: config.ignore, onlyDirectories: true });
     return Promise.all([targetDir, ...dirPaths].map(async dirPath => {
-        const childDirPaths = await io.glob('*', { cwd: dirPath, onlyDirectories: true });
+        const childDirPaths = await io.glob('*', { cwd: dirPath, onlyDirectories: true, markDirectories: true });
         const childFilePaths = await io.glob(config.only, { cwd: dirPath, onlyFiles: true, globstar: false });
         const filePaths = childDirPaths.concat(childFilePaths);
         return { dirPath, filePaths };
