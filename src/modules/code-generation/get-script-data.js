9bbf28649da0a7ec8ca3741ec769cc346be53043
Matt Riley <mattrileyaus@gmail.com>
2022-04-26 20:31:45 +1000

    refactor

--- src/modules/code-generation/get-script-data.js
+++ src/modules/code-generation/get-script-data.js
@@ -3,7 +3,7 @@ module.exports = ({ codeGeneration, providers, config }) => dirData => {
     const { dirPath, childPaths } = dirData;
     const childDataList = childPaths.map(childPath => codeGeneration.getFileData({ childPath }));
     const files = Object.fromEntries(childDataList.map(({ key, importPath }) => [key, importPath]));
-    const script = providers[config.type].generateScript({ files });
+    const script = providers[config.type]({ files });
     return { dirPath, script };
 
 };
