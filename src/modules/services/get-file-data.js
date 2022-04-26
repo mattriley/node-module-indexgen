abb29b70cdcb79eab4c24c1684df7c760db9b5de
Matt Riley <mattrileyaus@gmail.com>
2022-04-26 22:14:38 +1000

    refactor

--- src/modules/services/get-file-data.js
+++ src/modules/services/get-file-data.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const camelCase = require('lodash.camelcase');
 
+const trimLeadingIllegalCharacters = str => str.replace(/^[^$_a-z]/i, '');
+
 module.exports = ({ util, config }) => ({ childPath }) => {
     const isDir = childPath.endsWith('/');
     const ext = path.extname(childPath);
     const basenameWithoutExt = util.getBasenameWithoutExt(childPath);
-    const keyCamel = camelCase(util.trimLeadingUnderscoresAndDigits(basenameWithoutExt));
-    const key = util.startsWithUpper(basenameWithoutExt) ? basenameWithoutExt : keyCamel;
+    const key1 = trimLeadingIllegalCharacters(basenameWithoutExt);
+    const keyCamel = camelCase(key1);
+    const key = util.startsWithUpper(key1) ? util.upperFirst(keyCamel) : keyCamel;
     const pathWithoutExt = `./${ext === '.json' ? childPath : basenameWithoutExt}`;
     const importPath = config.fullySpecified ? (isDir ? `./${childPath}${config.filename}` : `./${childPath}`) : pathWithoutExt;
     return { key, importPath };
--- src/modules/util/index.js
+++ src/modules/util/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     getBasenameWithoutExt: require('./get-basename-without-ext'),
     startsWithUpper: require('./starts-with-upper'),
-    trimLeadingUnderscoresAndDigits: require('./trim-leading-underscores-and-digits')
+    upperFirst: require('./upper-first')
 };
