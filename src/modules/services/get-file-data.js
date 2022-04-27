09dc34abc24c45b0bf6bc4dac950ee94412f4076
Matt Riley <mattrileyaus@gmail.com>
2022-04-27 11:04:20 +1000

    refactor

--- src/modules/services/get-file-data.js
+++ src/modules/services/get-file-data.js
@@ -3,15 +3,17 @@ const camelCase = require('lodash.camelcase');
 const trimLeadingIllegalCharacters = str => str.replace(/^[^$_a-z]/i, '');
 
 module.exports = ({ futil, util, config }) => ({ childPath }) => {
+
     const isDir = childPath.endsWith('/');
     const ext = futil.extname(childPath);
     const basenameWithoutExt = futil.basenameWithoutExt(childPath);
-    const key1 = trimLeadingIllegalCharacters(basenameWithoutExt);
-    const keyCamel = camelCase(key1);
-    const key = util.legalJsName(key1) ? key1 : (util.startsWithUpper(key1) ? util.upperFirst(keyCamel) : keyCamel);
+    const keyRaw = trimLeadingIllegalCharacters(basenameWithoutExt);
+    const keyCamel = camelCase(keyRaw);
+    const key = util.legalJsName(keyRaw) ? keyRaw : (util.startsWithUpper(keyRaw) ? util.upperFirst(keyCamel) : keyCamel);
     const fullySpecified = config.fullySpecified || ext === '.json';
     const pathWithoutExt = fullySpecified ? childPath : basenameWithoutExt;
     const fullySpecifiedImportPath = isDir ? `./${childPath}${config.filename}` : `./${childPath}`;
     const importPath = fullySpecified ? fullySpecifiedImportPath : `./${pathWithoutExt}`;
     return { key, importPath };
+
 };
