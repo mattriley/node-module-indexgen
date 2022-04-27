25c0bbc656e4c44133003c4ec7c748ccac3a6925
Matt Riley <mattrileyaus@gmail.com>
2022-04-27 10:14:21 +1000

    refactor

--- src/modules/services/get-file-data.js
+++ src/modules/services/get-file-data.js
@@ -9,7 +9,9 @@ module.exports = ({ futil, util, config }) => ({ childPath }) => {
     const key1 = trimLeadingIllegalCharacters(basenameWithoutExt);
     const keyCamel = camelCase(key1);
     const key = util.legalJsName(key1) ? key1 : (util.startsWithUpper(key1) ? util.upperFirst(keyCamel) : keyCamel);
-    const pathWithoutExt = `./${ext === '.json' ? childPath : basenameWithoutExt}`;
-    const importPath = config.fullySpecified ? (isDir ? `./${childPath}${config.filename}` : `./${childPath}`) : pathWithoutExt;
+    const fullySpecified = config.fullySpecified || ext === '.json';
+    const pathWithoutExt = fullySpecified ? childPath : basenameWithoutExt;
+    const fullySpecifiedImportPath = isDir ? `./${childPath}${config.filename}` : `./${childPath}`;
+    const importPath = fullySpecified ? fullySpecifiedImportPath : `./${pathWithoutExt}`;
     return { key, importPath };
 };
