b883ce45c3fdbe9d340b88368417d407979cc362
Matt Riley <mattrileyaus@gmail.com>
2022-05-13 20:32:16 +1000

    detect module type in package json

--- bin/indexgen.js
+++ bin/indexgen.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 
 const minimist = require('minimist');
-const compose = require('../src/compose');
 const process = require('process');
+const package = require('./package.json');
+const compose = require('../src/compose');
+
 const { _, ...args } = minimist(process.argv.slice(2));
+const type = package.type === 'module' ? 'esm' : 'cjs';
 const paths = _.filter(p => p.length);
+const config = { paths, type, ...args };
 
 const parts = [
     args.watch ? 'Watching' : 'Generating',
     ...paths.map(p => `"${p}"`)
 ];
 
+if (args.showConfig) console.log(JSON.stringify(config, null, 2))
 console.log(parts.join(' '));
-const { commands } = compose(args);
+
+const { commands } = compose(config);
 paths.forEach(dir => commands.indexgen(dir));
 if (args.watch) paths.forEach(dir => commands.watch(dir));
