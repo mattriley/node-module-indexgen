3b8ddff4a25a38a1b63e9897fb9f9e10de2b5158
Matt Riley <mattrileyaus@gmail.com>
2022-07-06 20:29:31 +1000

    Unspecified changes

--- bin/indexgen.js
+++ bin/indexgen.js
@@ -2,23 +2,12 @@
 
 const minimist = require('minimist');
 const process = require('process');
-const package = require(process.cwd() + '/package.json');
 const compose = require('../src/compose');
 
 const { _, ...args } = minimist(process.argv.slice(2));
-const type = package.type === 'module' ? 'esm' : 'cjs';
-const paths = _.filter(p => p.length);
-const configs = [{ paths, type, ...args }];
-
-const parts = [
-    args.watch ? 'Watching' : 'Generating',
-    ...paths.map(p => `"${p}"`)
-];
-
-if (args.showConfig) console.log(JSON.stringify(config, null, 2))
-console.log(parts.join(' '));
-
-const { modules } = compose({ configs });
-const { commands } = modules;
+const paths = _.filter(path => path.length);
+const configs = [{ ...args, paths }];
+const { config, modules: { commands } } = compose({ configs });
+console.log(config);
 paths.forEach(dir => commands.indexgen(dir));
 if (args.watch) paths.forEach(dir => commands.watch(dir));
