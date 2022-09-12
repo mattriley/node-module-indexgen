9bf295d1398b3d2447f9343e1d6e1d85cb908a5e
Matt Riley <m@ttriley.dev>
2022-09-12 21:27:33 +1000

    Unspecified changes

--- src/modules/effects/get-file-data.js
+++ src/modules/effects/get-file-data.js
@@ -6,11 +6,14 @@ module.exports = ({ futil, util, config }) => pathname => {
     const isDir = pathname.endsWith('/');
     const ext = futil.extname(pathname);
     const basenameWithoutExt = futil.basenameWithoutExt(pathname);
-    const keyRaw = trimLeadingIllegalCharacters(basenameWithoutExt);
+    const keyRaw1 = trimLeadingIllegalCharacters(basenameWithoutExt);
+
+    const [, leadingSymbols = '', keyRaw] = keyRaw1.match(/(^[$_]+)?(.+)/);
+
     const keyCamel = camelCase(keyRaw);
     const keyTransformed = util.startsWithUpper(keyRaw) ? util.upperFirst(keyCamel) : keyCamel;
     const keyRawOrTransformed = util.legalJsName(keyRaw) ? keyRaw : keyTransformed;
-    const key = config.transformKeys ? keyRawOrTransformed : keyRaw;
+    const key = leadingSymbols + (config.transformKeys ? keyRawOrTransformed : keyRaw);
     const fullySpecified = config.fullySpecified || ext === '.json';
     const pathWithoutExt = fullySpecified ? pathname : basenameWithoutExt;
     const fullySpecifiedImportPath = isDir ? pathname + config.filename : pathname;
