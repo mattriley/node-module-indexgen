91fe25027a0497898eb4137c4fae16362cb39e7c
Matt Riley <m@ttriley.dev>
2023-05-22 08:44:29 +1000

    Unspecified changes

--- src/modules/lib/get-export-key.js
+++ src/modules/lib/get-export-key.js
@@ -14,20 +14,3 @@ module.exports = ({ util }) => (pathname, config) => {
     return exportKey;
 
 };
-
-
-// const camelCase = require('lodash.camelcase');
-
-// module.exports = ({ util }) => (pathname, config) => {
-
-//     const basenameWithoutExt = util.basenameWithoutExt(pathname);
-//     const keyMinusLeadingIllegalChars = util.trimLeadingIllegalJsChars(basenameWithoutExt);
-//     const [, leadingSymbols = '', keyAlpha] = keyMinusLeadingIllegalChars.match(/(^[$_]+)?(.+)/);
-//     const keyMaybeReversed = config.commaReverse ? keyAlpha.split(',').reverse().join(' ') : keyAlpha;
-//     const keyCamel = camelCase(keyMaybeReversed);
-//     const keyMaybeUpperFirst = util.startsWithUpper(keyAlpha) ? util.upperFirst(keyCamel) : keyCamel;
-//     const keyAlphaOrTransformed = util.legalJsName(keyAlpha) ? keyAlpha : keyMaybeUpperFirst;
-//     const exportKey = leadingSymbols + (config.transformKeys ? keyAlphaOrTransformed : keyAlpha);
-//     return exportKey;
-
-// };
