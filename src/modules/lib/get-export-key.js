07fe69677261f3c21839dc0aa42df1c2a6f34090
Matt Riley <m@ttriley.dev>
2023-05-22 17:18:59 +1000

    Unspecified changes

--- src/modules/lib/get-export-key.js
+++ src/modules/lib/get-export-key.js
@@ -4,13 +4,14 @@ module.exports = ({ util }) => (pathname, config) => {
 
     const basenameMinusExtension = util.basenameWithoutExt(pathname);
     const [basenameMinusSort] = basenameMinusExtension.split(config.sortSeparator).reverse();
+    const keyBare = basenameMinusSort;
     const [, leadingSymbols = '', keyAlpha] = basenameMinusSort.match(/(^[$_]+)?(.+)/);
     const keyMaybeReversed = keyAlpha.split(config.reverseDelimiter).reverse().join(' ');
     const keyBeforeCamel = keyMaybeReversed;
     const keyCamel = camelCase(keyMaybeReversed);
     const keyMaybeUpperFirst = util.startsWithUpper(keyBeforeCamel) ? util.upperFirst(keyCamel) : keyCamel;
     const keyAlphaOrTransformed = util.legalJsName(keyBeforeCamel) ? keyAlpha : keyMaybeUpperFirst;
-    const exportKey = leadingSymbols + (config.transformKeys ? keyAlphaOrTransformed : keyAlpha);
+    const exportKey = config.transformKeys ? leadingSymbols + keyAlphaOrTransformed : keyBare;
     return exportKey;
 
 };
