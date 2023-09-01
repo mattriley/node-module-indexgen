4c6009de1a6b964f94f889010c4744f7318f0472
Matt Riley <m@ttriley.dev>
2023-09-02 09:06:13 +1000

    Unspecified changes

--- src/compose.js
+++ src/compose.js
@@ -6,13 +6,12 @@ module.exports = ({ overrides, config }) => {
 
     const { configure } = composer(modules, { overrides });
 
-    const { compose } = configure([defaultConfig, config, config => {
-        const fullySpecified = config.fullySpecified ?? config.type === 'esm';
-        return { fullySpecified };
-    }]);
+    const { compose } = configure(defaultConfig, config, config => {
+        return { fullySpecified: config.fullySpecified ?? config.type === 'esm' };
+    });
 
-    const { io } = compose('io', {}, io => io.setup());
-    const { util } = compose('util', {});
+    const { io } = compose('io');
+    const { util } = compose('util');
     const { strategies } = compose('strategies', { util });
     const { lib } = compose('lib', { strategies, util });
     const { fsx } = compose('fsx', { io });
