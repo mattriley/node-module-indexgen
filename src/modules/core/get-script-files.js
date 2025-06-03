1de7a754a530faf9392bf8e936ea567a188a2d56
Matt Riley <m@ttriley.dev>
2025-06-03 14:52:47 +1000

    Unspecified changes

---
+++ src/modules/core/get-script-files.js
@@ -0,0 +1,23 @@
+module.exports = ({ self, config }) => dirData => {
+
+    const { childPaths } = dirData;
+    const configOverride = config.overrides?.[dirData.targetDir] ?? {};
+    const configFinal = { ...config, ...configOverride };
+
+    const childDataList = childPaths.map(childPath => {
+        const isFile = dirData.childFiles.includes(childPath);
+        const exportKey = self.getExportKey(childPath, configFinal, { isFile });
+        const importPath = self.getImportPath(childPath, configFinal);
+        return { exportKey, importPath };
+    });
+
+    const sortFiles = childDataList => {
+        const filesByPath = Object.fromEntries(childDataList.map(f => [f.importPath, f]));
+        const collator = new Intl.Collator([], { numeric: true });
+        const sortedPaths = Object.keys(filesByPath).sort((a, b) => collator.compare(a, b));
+        return sortedPaths.map(path => filesByPath[path]);
+    }
+
+    return sortFiles(childDataList);
+
+};
--- src/modules/core/get-script.js
+++ src/modules/core/get-script.js
@@ -1,24 +1,6 @@
 module.exports = ({ self, strategies, config }) => dirData => {
 
-    const { childPaths } = dirData;
-    const configOverride = config.overrides?.[dirData.targetDir] ?? {};
-    const configFinal = { ...config, ...configOverride };
-
-    const childDataList = childPaths.map(childPath => {
-        const isFile = dirData.childFiles.includes(childPath);
-        const exportKey = self.getExportKey(childPath, configFinal, { isFile });
-        const importPath = self.getImportPath(childPath, configFinal);
-        return { exportKey, importPath };
-    });
-
-    const sortFiles = childDataList => {
-        const filesByPath = Object.fromEntries(childDataList.map(f => [f.importPath, f]));
-        const collator = new Intl.Collator([], { numeric: true });
-        const sortedPaths = Object.keys(filesByPath).sort((a, b) => collator.compare(a, b));
-        return sortedPaths.map(path => filesByPath[path]);
-    }
-
-    const files = sortFiles(childDataList);
+    const files = self.getScriptFiles(dirData);
     const script = strategies[config.type]({ files });
     return script;
 
