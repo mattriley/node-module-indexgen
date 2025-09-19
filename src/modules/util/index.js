72452ebef7cad4a24edc31d288f056f77ace75b2
Matt Riley <m@ttriley.dev>
2025-09-19 21:45:27 +1000

    Unspecified changes

--- src/modules/util/index.js
+++ src/modules/util/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     basenameWithoutExt: require('./basename-without-ext'),
-    extname: require('./extname'),
     legalJsName: require('./legal-js-name'),
     startsWithUpper: require('./starts-with-upper'),
     trimLeadingIllegalJsChars: require('./trim-leading-illegal-js-chars'),
