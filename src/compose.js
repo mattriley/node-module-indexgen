46311a71ee7aeca6ea80e1b5b56bead885a1e9c9
Matt Riley <m@ttriley.dev>
2025-09-19 22:58:35 +1000

    Unspecified changes

--- src/compose.js
+++ src/compose.js
@@ -1,18 +1,15 @@
-const normalise = require('./normalise.js');
-const defaults = require('./defaults.js');
-const modules = require('./modules');
 const composer = require('module-composer');
+const modules = require('./modules');
+const defaults = require('./defaults.js');
+const normalise = require('./normalise.js');
 
 module.exports = ({ overrides, config } = {}) => {
-
     const { configure } = composer(modules, { overrides });
     const { compose } = configure(defaults, config, normalise);
-
     const { io } = compose('io');
     const { util } = compose('util');
     const { renderers } = compose('renderers', { util });
     const { fsx } = compose('fsx', { io, util });
     const { core } = compose('core', { renderers, util });
     return compose('commands', { core, fsx, io });
-
 };
