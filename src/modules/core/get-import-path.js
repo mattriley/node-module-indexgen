3f9f22c8af9cc245fd8c34fd6eca970bc7b84223
Matt Riley <m@ttriley.dev>
2025-09-19 13:02:53 +1000

    Unspecified changes

--- src/modules/core/get-import-path.js
+++ src/modules/core/get-import-path.js
@@ -1,13 +1,34 @@
+const path = require('path');
+
 module.exports = ({ util }) => (pathname, config) => {
+    const EXT_SET = new Set(config.applicableExtensions.map(e => e.toLowerCase()));
+
+    const parsed = path.parse(pathname);
+    const cleanExt = parsed.ext.slice(1).toLowerCase();
+    const hasKnownExt = EXT_SET.has(cleanExt);
+
+    const basenameWithoutExt = hasKnownExt
+        ? path.format({ ...parsed, base: undefined, ext: '' })
+        : pathname;
 
     const isDir = pathname.endsWith('/');
-    const ext = util.extname(pathname);
-    const basenameWithoutExt = util.basenameWithoutExt(pathname);
-    const fullySpecified = config.fullySpecified || ext === '.json';
-    const pathWithoutExt = fullySpecified ? pathname : basenameWithoutExt;
-    const fullySpecifiedImportPath = isDir ? pathname + config.filename : pathname;
-    const pathUnqualified = fullySpecified ? fullySpecifiedImportPath : pathWithoutExt;
-    const pathQualified = `./${pathUnqualified}`;
-    return pathQualified;
+    const fullySpecified = config.fullySpecified || cleanExt === 'json';
 
+    let pathUnqualified;
+
+    if (fullySpecified) {
+        // keep slash for dirs, append filename
+        pathUnqualified = isDir
+            ? pathname + config.filename
+            : pathname;
+    } else {
+        // strip extension if applicable
+        pathUnqualified = basenameWithoutExt;
+        // also strip trailing slash for dirs
+        if (isDir && pathUnqualified.endsWith('/')) {
+            pathUnqualified = pathUnqualified.slice(0, -1);
+        }
+    }
+
+    return `./${pathUnqualified}`;
 };
