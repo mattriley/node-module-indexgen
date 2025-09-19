8d7bf43bc0ac635eb870f4d888f6cae74e444b42
Matt Riley <m@ttriley.dev>
2025-09-19 17:31:48 +1000

    Unspecified changes

--- src/modules/core/index.js
+++ src/modules/core/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    $renderScripts: require('./$render-scripts'),
+    renderScripts: require('./render-scripts'),
     getExportName: require('./get-export-name'),
     getImportPath: require('./get-import-path'),
     getScriptFiles: require('./get-script-files'),
