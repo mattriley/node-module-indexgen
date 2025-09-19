7669353a94d54c8e702f46e5604445cf243a49ed
Matt Riley <m@ttriley.dev>
2025-09-19 18:52:13 +1000

    Unspecified changes

--- src/modules/core/get-script-files.js
+++ src/modules/core/get-script-files.js
@@ -3,8 +3,7 @@ module.exports = ({ self, config }) => dirData => {
     const configFinal = config.overrides[dirData.targetDir] ?? config;
 
     const childDataList = dirData.sortedChildPaths.map(childPath => {
-        const isFile = dirData.childFiles.includes(childPath);
-        const exportName = self.getExportName(childPath, configFinal, { isFile });
+        const exportName = self.getExportName(childPath, configFinal);
         const importPath = self.getImportPath(childPath, configFinal);
         return { exportName, importPath };
     });
